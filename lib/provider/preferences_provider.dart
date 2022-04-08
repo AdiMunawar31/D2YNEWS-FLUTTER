adf6497c8d2652d9cc1a0f3bf039ed0a9d302fac
Adi Munawar <munawar.adi31@gmail.com>
2022-04-08 09:45:42 +0700

    implements shared preferences

---
+++ lib/provider/preferences_provider.dart
@@ -0,0 +1,37 @@
+import 'package:d2ynews/data/preferences/preferences_helper.dart';
+import 'package:flutter/foundation.dart';
+
+class PreferencesProvider extends ChangeNotifier {
+  late PreferencesHelper preferencesHelper;
+
+  PreferencesProvider({required this.preferencesHelper}) {
+    _getTheme();
+    _getDailyNewsPreferences();
+  }
+
+  bool _isDarkTheme = false;
+  bool get isDarkTheme => _isDarkTheme;
+
+  bool _isDailyNewsActive = false;
+  bool get isDailyNewsActive => _isDailyNewsActive;
+
+  void _getTheme() async {
+    _isDarkTheme = await preferencesHelper.isDarkTheme;
+    notifyListeners();
+  }
+
+  void _getDailyNewsPreferences() async {
+    _isDailyNewsActive = await preferencesHelper.isDailyNewsActive;
+    notifyListeners();
+  }
+
+  void enableDarkTheme(bool value) {
+    preferencesHelper.setDarkTheme(value);
+    _getTheme();
+  }
+
+  void enableDailyNews(bool value) {
+    preferencesHelper.setDailyNews(value);
+    _getDailyNewsPreferences();
+  }
+}
